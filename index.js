62a41d7558c4767d8a1029344c3fc686e7523aea
Didier Irias Méndez <didier.irias@gmail.com>
2021-10-23 07:32:21 -0600

    small test server created

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Estarting project');
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (request, response) =>{
+  response.send('This is the home page');
+});
+
+app.get('/about', (request, response) =>{
+  response.send('This is about');
+});
+app.get('/products', (request, response) =>{
+  response.json({
+    name: 'Product 1',
+    price: 3500
+  });
+});
+
+app.listen(port, () =>{
+  console.log('Running on port ' + port);
+});
